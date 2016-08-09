364f2cd83d040abd322999ed36386c4d0045964b
Renato Moura <renato@naturalweb.com.br>
2016-08-09 12:59:07 -0300

    Created test unit Exceptions:  - BatchRequestExceptionTest.php  - InvalidBalancerUrlExceptionTest.php  - NetworkErrorExceptionTest.php  - UnauthorizedExceptionTest.php

---
+++ tests/Exceptions/InvalidBalancerUrlExceptionTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Tests\Exceptions;
+
+use Tests\TestCase;
+use Nikapps\OrtcPhp\Exceptions\InvalidBalancerUrlException;
+
+class InvalidBalancerUrlExceptionTest extends TestCase
+{
+    public function testImplements()
+    {
+        $exception = new InvalidBalancerUrlException;
+        $this->assertEquals("Balancer URL is invalid", $exception->getMessage());
+
+        $this->assertAttributeEquals(null, 'url', $exception);
+        $this->assertEquals(null, $exception->getUrl());
+
+        $url = ['value' => 1];
+        $exception->setUrl($url);
+        $this->assertAttributeEquals($url, 'url', $exception);
+        $this->assertEquals($url, $exception->getUrl());
+    }
+}
